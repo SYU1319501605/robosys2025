1f59edeac2e49899cf761d042da4e44c56823790
Kaki Syu <zhu1319501605@icloud.com>
2025-11-24 15:31:38 +0900

    feat: add chrcount command and update tests

--- test.bash
+++ test.bash
@@ -2,28 +2,32 @@
 # SPDX-FileCopyrightText: 2025 Kaki Syu
 # SPDX-License-Identifier: BSD-3-Clause
 
-
 ng(){
       echo ${1}行目が違うよ
       res=1
 }
+
 res=0
 
-
 ### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng "$LINENO"
+out=$(echo 'abc123XYZ!?@' | ./chrcount)
+[ "$(echo "$out" | grep -i total | awk '{print $2}')" = 12 ] || ng "$LINENO"
+[ "$(echo "$out" | grep -i alphabets | awk '{print $2}')" = 6 ] || ng "$LINENO"
+[ "$(echo "$out" | grep -i digits | awk '{print $2}')" = 3 ] || ng "$LINENO"
+[ "$(echo "$out" | grep -i others | awk '{print $2}')" = 3 ] || ng "$LINENO"
 
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
+out=$(echo | ./chrcount)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo  | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
+out=$(echo あいう | ./chrcount)
+[ "$?" = 0 ] || ng "$LINENO"  # エラーにならない
+[ "$(echo "$out" | grep -i total | awk '{print $2}')" = 3 ] || ng "$LINENO"
+[ "$(echo "$out" | grep -i alphabets | awk '{print $2}')" = 0 ] || ng "$LINENO"
+[ "$(echo "$out" | grep -i digits | awk '{print $2}')" = 0 ] || ng "$LINENO"
+[ "$(echo "$out" | grep -i others | awk '{print $2}')" = 3 ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
